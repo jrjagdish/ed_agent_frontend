b4558ddc196c07b8709f3ca37b2ca0ac46c1f521
jrjagdish <hanamshettyjagdish1@gmail.com>
2026-02-22 11:28:11 +0530

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic'   // 👈 THIS IS THE FIX
-    })
-  ]
-})
+      jsxRuntime: "automatic", // 👈 THIS IS THE FIX
+    }),
+    tailwindcss(),
+  ],
+});
